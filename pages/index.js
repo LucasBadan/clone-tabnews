40bcd5d0c4554784da3a90f379fb3651e98eaf43
Lucas Badan <160783502+LucasBadan@users.noreply.github.com>
2025-04-28 02:05:34 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-function Home(){
-    return <h1>Teste</h1>
-    
+function Home() {
+  return <h1>Teste</h1>;
 }
 
-export  default Home;
+function teste() {
+  console.log("Teste");
+}
+
+export default Home;
